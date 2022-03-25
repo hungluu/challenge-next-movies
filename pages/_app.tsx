28981bf496dbb50ed8f390d3694c5bb4bf18d0e1
Hung Luu <hungluu2106.com>
2022-03-25 13:15:32 +0700

    chore: Add more tools into source code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import pkg from '../package.json'
+import 'normalize.css/normalize.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>{pkg.name.replace(/[\W]+/, ' ')}</title>
+      <meta name='description' content={pkg.description} />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
---
+++ pages/_document.tsx
@@ -0,0 +1,39 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  render () {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+
+  static async getInitialProps (ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: Function) => (props: any) => sheet.collectStyles(<App {...props} />)
+        })
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}
